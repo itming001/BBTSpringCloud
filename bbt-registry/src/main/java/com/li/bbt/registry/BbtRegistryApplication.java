9f8ada6a728b7616d2368fbbf8bee75a0b784008
liyanming <13315100614@163.com>
2019-12-29 22:33:19 +0800

    增加注解

--- bbt-registry/src/main/java/com/li/bbt/registry/BbtRegistryApplication.java
+++ bbt-registry/src/main/java/com/li/bbt/registry/BbtRegistryApplication.java
@@ -8,6 +8,6 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 public class BbtRegistryApplication {
     public static void main(String[] args) {
-        SpringApplication.run(BbtRegistryApplication.class,args);
+        SpringApplication.run(BbtRegistryApplication.class, args);
     }
 }
