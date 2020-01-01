0ac2c4fe8b9378c975a9ccc3fa1f33fe2615b5a6
liyanming <13315100614@163.com>
2020-01-01 11:00:08 +0800

    增加自定义pom文件和增加公共的返回配置类

---
+++ bbt-common/src/main/java/com/li/bbt/common/util/R.java
@@ -0,0 +1,75 @@
+package com.li.bbt.common.util;
+
+import com.li.bbt.common.constant.CommonConstants;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Accessors(chain = true)
+public class R<T> implements Serializable {
+    private static final long serialVersionUID = 1L;
+    /**
+     * 返回标记：成功标记=0，失败标记=1
+     */
+    @Getter
+    @Setter
+    private int code;
+    /**
+     * 返回信息
+     */
+    @Getter
+    @Setter
+    private String msg;
+
+    /**
+     * 数据
+     */
+    @Getter
+    @Setter
+    private T data;
+
+    public static <T> R<T> ok() {
+        return restResult(null, CommonConstants.SUCCESS, null);
+    }
+
+    public static <T> R<T> ok(T data) {
+        return restResult(data, CommonConstants.SUCCESS, null);
+    }
+
+    public static <T> R<T> ok(T data, String msg) {
+        return restResult(data, CommonConstants.SUCCESS, msg);
+    }
+
+    public static <T> R<T> failed() {
+        return restResult(null, CommonConstants.FAIL, null);
+    }
+
+    public static <T> R<T> failed(String msg) {
+        return restResult(null, CommonConstants.FAIL, msg);
+    }
+
+    public static <T> R<T> failed(T data) {
+        return restResult(data, CommonConstants.FAIL, null);
+    }
+
+    public static <T> R<T> failed(T data, String msg) {
+        return restResult(data, CommonConstants.FAIL, msg);
+    }
+
+    private static <T> R<T> restResult(T data, int code, String msg) {
+        R<T> apiResult = new R<>();
+        apiResult.setCode(code);
+        apiResult.setData(data);
+        apiResult.setMsg(msg);
+        return apiResult;
+    }
+}
+
